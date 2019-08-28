d6cab6783212e8687267028e7899f22906352787
Ryan Glacock <rmggz4@mail.missouri.edu>
2019-08-28 09:19:55 -0500

    First Commit

---
+++ Documents/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Documents
+//
+//  Created by Ryan Glascock on 8/28/19.
+//  Copyright © 2019 Ryan Glascock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
